9736adec26870b096a0bb484c8ac05dcdee2f619
shikhar <shikhar21121999@gmail.com>
2020-12-04 01:35:44 +0530

    todays work

---
+++ linked_list_template.cpp
@@ -0,0 +1,177 @@
+// template
+#include <bits/stdc++.h>
+ 
+# define C continue
+# define R return
+ 
+# define D double
+# define I insert
+# define ll long long
+# define ld long double
+ 
+# define ull unsigned long long
+# define ui unsigned int
+ 
+# define pb push_back
+# define pf push_front
+ 
+# define vi vector < int >
+# define vc vector < char >
+# define vs vector < string >
+# define vb vector < bool >
+# define vd vector < D >
+# define vll vector < ll >
+# define vull vector < ull >
+# define vld vector < ld >
+# define PQ priority_queue
+ 
+# define vvi vector < vector < int > >
+# define vvb vector < vector < bool > >
+# define vvc vector < vector < char > >
+# define vvll vector < vector < ll > >
+# define vvd vector < vector < D > >
+# define vvld vector < vector < ld > >
+ 
+# define all(v) (v).begin() , (v).end()
+# define allrev(v) (v).rbegin() , (v).rend()
+# define allcomp(v) v.begin() , v.end() , comp
+# define allrevcomp(v) v.rbegin() , v.rend() , comp
+ 
+# define pii pair < int , int >
+# define pll pair < ll , ll >
+# define pld pair < ld , ld >
+# define pDD pair < D , D >
+# define pipii pair<int, pii >
+# define pcpii pair<char,pii>
+ 
+# define vpld vector < pld >
+# define vpii vector < pii >
+# define vpll vector < pll >
+# define vpDD vector < pDD >
+ 
+# define vvpii vector < vector < pii > >
+# define vpipii vector< pipii >
+ 
+# define F first
+# define S second
+# define mp make_pair
+ 
+# define unm unordered_map
+# define unmii unm< int , int >
+# define unmll unm< ll , ll >
+# define unms unm<string,int>
+# define unmci unm<char,int>
+ 
+# define sortvia sort(a.begin(),a.end());
+# define sortvib sort(b.begin(),b.end());
+# define revsorta sort(a.begin(), a.end(), greater <int>());
+# define revsortb sort(b.begin(), b.end(), greater <>());
+# define loop(q,n) for(int i=q;i<n;i++)
+# define loop2(q,n) for(int j=q;j<n;j++)
+# define test int t;cin >> t;while(t--)
+# define nextline "\n"
+# define tab "\t"
+# define space " "
+# define get_arr_size_n int n;cin>>n;int arr[n];loop(0,n)cin>>arr[i];
+//vector<vector<int> > vec( n , vector<int> (m, 0));
+//YES
+//NO
+//cout
+//true
+//false
+//yes
+//no
+ 
+const ll mod9=1e9+7;
+const ll maxsize=2e9+1;
+// const ll mod =998244353;
+const ll mod2=1073741824;
+const ll INF=1e18L+5;
+const int IINF=1e9+5;
+const int two_pow_fiv=200008;
+using namespace std;
+
+/* Link list node */
+class Node  
+{  
+    public: 
+    int data;  
+    Node* next;  
+};  
+
+
+/* Reverses the linked list in groups 
+of size k and returns the pointer 
+to the new head node. */
+Node *reverse (Node *head, int k)  
+{  
+    Node* current = head;  
+    Node* next = NULL;  
+    Node* prev = NULL;  
+    int count = 0;  
+      
+    /*reverse first k nodes of the linked list */
+    while (current != NULL && count < k)  
+    {  
+        next = current->next;  
+        current->next = prev;  
+        prev = current;  
+        current = next;  
+        count++;  
+    }  
+      
+    /* next is now a pointer to (k+1)th node  
+    Recursively call for the list starting from current.  
+    And make rest of the list as next of first node */
+    if (next != NULL)  
+    head->next = reverse(next, k);  
+  
+    /* prev is new head of the input list */
+    return prev;  
+}  
+  
+/* UTILITY FUNCTIONS */
+/* Function to push a node */
+void push(Node** head_ref, int new_data)  
+{  
+    /* allocate node */
+    Node* new_node = new Node(); 
+  
+    /* put in the data */
+    new_node->data = new_data;  
+  
+    /* link the old list off the new node */
+    new_node->next = (*head_ref);      
+  
+    /* move the head to point to the new node */
+    (*head_ref) = new_node;  
+}  
+  
+/* Function to print linked list */
+void printList(Node *node)  
+{  
+    while (node != NULL)  
+    {  
+        cout<<node->data<<" ";  
+        node = node->next;  
+    }  
+}
+
+
+
+void solve(){
+
+}
+
+
+int main()
+{
+ios::sync_with_stdio(0);
+cin.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+#endif
+    solve();
+return 0;
+}
