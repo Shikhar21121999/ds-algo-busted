89aee51627d023aada33c0da5f4deae1c3340c8d
shikharsun <shikhar21121999@gmail.com>
2021-03-03 23:15:59 +0530

    work

--- detect_cycle.cpp
+++ detect_cycle.cpp
@@ -1,4 +1,4 @@
-// program to detect cycle in a graph
+// program to detect cycle in a directed graph
 // link:https://www.geeksforgeeks.org/detect-cycle-in-a-graph/
 
 #include <bits/stdc++.h>
@@ -7,34 +7,53 @@ using namespace std;
 #define vi vector<int>
 #define pii pair<int,int>
 #define vpii vector<pii>
+#define vvpii vector<vpii>
 
-void dfs()
+// use dfs to detect cycle
+void dfs(int node,vvpii &adj,bool &cycle,vi &vis){
+	if(cycle)return;
+	
+	// perform dfs on the current node
+	for(auto x:adj[node]){
+		if(vis[x.first]){
+			cycle=true;
+		}
+		else{
+			vis[x.first]=true;
+			dfs(x.first,adj,cycle,vis);
+		}
+	}
+	return;
+}
 
-bool is_cyclic(vpii &adj,int v){
+
+bool is_cyclic(vvpii &adj,int v){
 	// function to check if the graph is cyclic or not
 	
 	vi vis(v,0);
+	bool cycle=false;
 	for(int i=0;i<v;i++){
 		if(!vis[i]){
 			vis[i]=1;
-			dfs(adj,i);
+			dfs(i,adj,cycle,vis);
 		}
 	}
+	return cycle;
 			
 }
 	
 
-void solve(){
+void input_adj_graph(){
 	// input no of vertex and edge
 	int v,e;
 	cin>>v>>e;
 	
 	// adjacency list representation of graph
-	vpii adj(v+1);
+	vvpii adj(v+1);
 	
 	// take in e edges
 	int v1,v2,edg;
-	for(int i=0;i<v;i++){
+	for(int i=0;i<e;i++){
 		cin>>v1>>v2>>edg;
 		adj[v1].push_back(make_pair(v2,edg));	
 	}
@@ -45,8 +64,9 @@ void solve(){
 			cout<<i<<"\t"<<x.first<<"\t"<<x.second<<"\n";
 		}
 	}
+	cout<<is_cyclic(adj,v);
 }
 
 int main(){
-	solve();
+	input_adj_graph();
 }
