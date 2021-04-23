a8ffa25874b7ac81b460c51d58c881240ee17e2b
shikharsun <shikhar21121999@gmail.com>
2021-04-23 22:41:37 +0530

    todays work

--- interleaving-string.cpp
+++ interleaving-string.cpp
@@ -3,24 +3,35 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+
+int dp[1001][1001];
 bool recur(int i,int j,int k,string a,string b,string c){
-	// base case
-	if(i==a.length() and j==b.length() and k==c.length()){
-		return true;
+	if(k==0)return 1;
+	if(dp[i][j]!=-1){
+		return dp[i][j];
 	}
-	// condition 1 when current character of a matches with c
-	bool cndn1=false;
-	if(i<a.length() and a[i]==c[k] and recur(i+1,j,k+1,a,b,c)){
-		return true;
-	}
-	if(j<b.length() and b[j]==c[k] and recur(i,j+1,k+1,a,b,c)){
-		return true;
-	}
-	return false;
+	int p,q;
+	p=q=0;
+	if(i-1>=0 and a[i-1]==c[k-1])p=recur(i-1,j,k-1,a,b,c);
+	if(j-1>=0 and b[j-1]==c[k-1])q=recur(i,j-1,k-1,a,b,c);
+	return dp[i][j]=p or q;
 }
+bool isInterleave(string A, string B, string C) 
+{
+    int n=A.length();
+    int m=B.length();
+    int len=C.length();
+    if(n+m!=len){
+    	return false;
+    }
+    dp[n][m];
+    memset(dp,-1,sizeof(dp));
+    return recur(n,m,len,A,B,C);
+}
+
 
 int main(){
 	string A,B,C;
 	cin>>A>>B>>C;
-	cout<<recur(0,0,0,A,B,C)<<endl;
+	cout<<isInterleave(A,B,C)<<endl;
 }
