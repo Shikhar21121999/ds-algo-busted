41b22fdceca1977e7353b56cbaff7e8146e489ce
shikharsun <shikhar21121999@gmail.com>
2021-04-29 23:02:35 +0530

    todays work

---
+++ pallindrome-linkedlist.cpp
@@ -0,0 +1,22 @@
+// program to identify wether a linked list is a pallindrome
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class ListNode{
+public:
+	ListNode* next;
+	int val;
+
+	// constructor
+	ListNode(){
+		this->next=nullptr;
+		this->val=0;
+	}
+};
+
+// function to find length of linked list
+// that is the number of nodes in the linked list
+int getLength(){
+	
+}
